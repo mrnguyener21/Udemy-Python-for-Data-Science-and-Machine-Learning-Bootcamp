db174737a5b87658c00e43c63fff0c69e14f1e64
mrnguyener21 <victortnguyen.workemail@gmail.com>
2020-11-25 20:35:11 -0800

    learned about numpy arrays

--- test.py
+++ test.py
@@ -1,1 +1,32 @@
-test
+#%%
+import numpy as np
+#numpy arrays
+np.arange(0,10)
+np.arange(0,11,2)
+
+# %%
+#single vector example
+np.zeros(2)
+# %%
+#matrix example
+np.zeros((2,3))
+# %%
+np.linspace(0,5,10)
+# %%
+np.eye(4)
+# %%
+np.random.rand(5)
+# %%
+np.random.rand(5,5)
+# %%
+np.random.randn(2)
+# %%
+np.random.randint(1,100,50)
+# %%
+arr = np.arange(25)
+arr
+# %%
+ranarr = np.random.randint(0,50,10)
+ranarr
+# %%
+arr.reshape(5,5)
