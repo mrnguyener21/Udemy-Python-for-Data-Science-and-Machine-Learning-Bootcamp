d8fd6e2ae3135303c952509fdc45ab37979c2547
mrnguyener21 <victortnguyen.workemail@gmail.com>
2020-11-28 11:58:07 -0800

    going through lesson 2 cnumpy array indexing

---
+++ section_5_python_for_data_analysis_numpy/lesson_2_numpy_array_indexing.py
@@ -0,0 +1,25 @@
+#%%
+import numpy as np
+arr = np.arange(0,11)
+# %%
+arr[8]
+# %%
+arr[1:5]
+# %%
+arr[0:5]
+# %%
+arr[:6]
+# %%
+arr[5:]
+# %%
+arr[0:5] = 100
+# %%
+arr
+# %%
+arr = np.arange(0,11)
+arr
+# %%
+slice_of_arr = arr[0:6]
+slice_of_arr
+# %%
+arr_copy = arr.copy() #you need to use the copy function because slice_of_arr currently is just a view, so changes to it will change the orignial array as well
