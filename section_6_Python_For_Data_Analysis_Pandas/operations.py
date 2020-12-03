da0862e79ee6ff5f90f16070a4a33f5bb194cac1
mrnguyener21 <victortnguyen.workemail@gmail.com>
2020-12-02 21:55:44 -0800

    learned about operations used in pandas

---
+++ section_6_Python_For_Data_Analysis_Pandas/operations.py
@@ -0,0 +1,22 @@
+#%%
+import numpy as np
+import pandas as pd 
+# %%
+df = pd.DataFrame({'col1':[1,2,3,4],'col2':[444,555,666,444],'col3':['abc','def','ghi','xyz']})
+df.head()
+# %%
+#there are three main value methods to find unique value
+df['col2'].unique() #returns the unique values
+# %%
+df['col2'].nunique()#returns the amount of unique values
+
+# %%
+df['col2'].value_counts()
+# %%
+df[df['col1'] > 2]
+# %%
+df['col1'] > 2
+# %%
+df[df['col1'] > 2 &( df['col2'] == 444)] 
+
+# %%
