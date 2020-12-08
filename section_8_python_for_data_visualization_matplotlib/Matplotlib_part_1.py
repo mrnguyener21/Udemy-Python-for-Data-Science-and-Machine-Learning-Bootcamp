95e275efa5d19ef692bdee6180020b865ea001c5
Victor Nguyen <vnguyen@triconah.com>
2020-12-08 15:20:14 -0800

    went through and commented matplotlib part 1

--- section_8_python_for_data_visualization_matplotlib/Matplotlib_part_1.py
+++ section_8_python_for_data_visualization_matplotlib/Matplotlib_part_1.py
@@ -11,19 +11,23 @@ x
 y
 # %%
 #FUNCTIONAL METHOD
+#here the plot function is used to create a basic graph with the function method
 plt.plot(x,y)
 # %%
+#created a basic graph along with a label for the x and y axis and title for the plot with the functional method
 plt.plot(x,y)
 plt.xlabel('X Label')
 plt.ylabel('Y Label')
 plt.title('Title')
 # %%
+#here we created two sub smaller charts within the main chart with their own graph with the functional method
 plt.subplot(1,2,1)
 plt.plot(x,y,'r')
 plt.subplot(1,2,2)
 plt.plot(y,x,'b')
 # %%
 #OBJECT ORIENTED METHOD(USE THIS METHOD)
+#here we created a graph with x and y axes along with labeling them and giving a title to the chart(this is done using object oriented method)
 fig = plt.figure()
 axes = fig.add_axes([0.1,0.1,0.8,0.8])
 axes.plot(x,y)
@@ -31,6 +35,7 @@ axes.set_xlabel('x label')
 axes.set_ylabel('y label')
 axes.set_title('Title')
 # %%
+#here we created a graph and a sub graph. The subgraph and large graph is determined on the size of the two graphs. Here axes1 have larger axes than axes 2 so it is the larger graph
 fig = plt.figure()
 axes1 = fig.add_axes([0.1,0.1,0.8,0.8])
 axes2 = fig.add_axes([0.2,0.5,0.4,0.3])
