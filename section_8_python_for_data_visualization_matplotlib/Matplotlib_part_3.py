09350c26541d3344149b75fa662e6156c446a4b8
mrnguyener21 <victortnguyen.workemail@gmail.com>
2020-12-08 09:59:25 -0800

    finished matplotlib part 3. pretty much how to make the graph looks nicer, think of css for it

---
+++ section_8_python_for_data_visualization_matplotlib/Matplotlib_part_3.py
@@ -0,0 +1,17 @@
+#%%
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.linspace(0,5,11)
+y = x ** 2
+
+fig = plt.figure()
+ax = fig.add_axes([0,0,1,1])
+ax.plot(x,y, color='orange',lw=1,alpha=0.8,ls='-',marker='o', markersize=10, markerfacecolor='green',markeredgewidth=3,markeredgecolor='black')
+# %%
+fig = plt.figure()
+ax = fig.add_axes([0,0,1,1])
+ax.plot(x,y, color='orange',lw=2,ls='-')
+ax.set_xlim([0,1])
+ax.set_ylim([0,2])
+# %%
