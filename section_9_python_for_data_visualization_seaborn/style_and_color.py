8daf213a4ed2ddf4fe2fc5dbe8c7884ce5c770a9
mrnguyener21 <victortnguyen.workemail@gmail.com>
2020-12-13 22:01:41 -0800

    finished seaborn exercises. not great

--- section_9_python_for_data_visualization_seaborn/style_and_color.py
+++ section_9_python_for_data_visualization_seaborn/style_and_color.py
@@ -3,3 +3,19 @@ import seaborn as sns
 tips = sns.load_dataset('tips')
 tips.head()
 # %%
+sns.countplot(x='sex',data=tips)
+# %%
+sns.set_style('white')
+sns.countplot(x='sex',data=tips)
+
+# %%
+sns.set_style('ticks')
+sns.countplot(x='sex',data=tips)
+# %%
+sns.set_style('darkgrid')
+sns.countplot(x='sex',data=tips)
+# %%
+sns.set_style('ticks')
+sns.countplot(x='sex',data=tips)
+sns.despine()
+# %%
